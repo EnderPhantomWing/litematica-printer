23bcc649e037c9fd364804b56e8c23d27e4a1761
EnderPhantomWing <EnderPhantomWing@outlook.com>
2026-05-22 00:59:07 +0800

    chore: artifacts update VIII

--- buildSrc/src/main/kotlin/ModProjectExtension.kt
+++ buildSrc/src/main/kotlin/ModProjectExtension.kt
@@ -59,6 +59,7 @@ val Project.fullProjectVersion: String get() = getFullProjectVersion(modArchives
 private fun getFullProjectVersion(modArchivesBaseName: String, modVersion: String): String {
     val isRelease = System.getenv("IS_THIS_RELEASE")?.toBoolean() == true
     val isCi = System.getenv("CI") == "true" || System.getenv("GITHUB_ACTIONS") == "true"
+    val commitHash = System.getenv("COMMIT_HASH")
 
     return when {
         isRelease -> modVersion
@@ -68,7 +69,7 @@ private fun getFullProjectVersion(modArchivesBaseName: String, modVersion: Strin
                 .format(Date())
                 .toString()
             val buildNumber = System.getenv("GITHUB_RUN_NUMBER")
-            val version = "v$modVersion+$time"
+            val version = "v$modVersion-$commitHash+$time"
             if (buildNumber != null) {
                 "$modArchivesBaseName-$version+build.$buildNumber"
             } else {
