1108891f53941fa7b45dd41fd610ffcbf7015174
EnderPhantomWing <EnderPhantomWing@outlook.com>
2026-05-22 21:04:08 +0800

    chore(ci): update action scripts VIII

--- .github/workflows/scripts/summary.py
+++ .github/workflows/scripts/summary.py
@@ -50,7 +50,7 @@ def main():
 				continue
 			game_versions = read_prop('versions/{}/gradle.properties'.format(subproject), 'game_versions')
 			game_versions = game_versions.strip().replace('\r', '').replace('\n', ', ')
-			file_paths = glob.glob('build-artifacts/{}/build/libs/*.jar'.format(subproject))
+			file_paths = glob.glob('build-artifacts/*.jar'.format(subproject))
 			file_paths = list(filter(lambda fp: not fp.endswith('-sources.jar') and not fp.endswith('-dev.jar') and not fp.endswith('-shadow.jar'), file_paths))
 			if len(file_paths) == 0:
 				file_name = '*not found*'
