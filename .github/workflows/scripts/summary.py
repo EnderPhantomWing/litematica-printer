ab5e013879a10c4386ccfda57290760b5d479559
EnderPhantomWing <EnderPhantomWing@outlook.com>
2026-05-22 22:21:20 +0800

    chore: update summary VI

--- .github/workflows/scripts/summary.py
+++ .github/workflows/scripts/summary.py
@@ -59,7 +59,7 @@ def main():
                 continue
             game_versions = read_prop('versions/{}/gradle.properties'.format(subproject), 'game_versions')
             game_versions = game_versions.strip().replace('\r', '').replace('\n', ', ')
-            file_paths = glob.glob('build-artifacts/tmp/submods/META-INF/jars/*.jar'.format(subproject), 'build-artifacts/libs/*.jar')
+            file_paths = glob.glob('build-artifacts/tmp/submods/META-INF/jars/*.jar'.format(subproject))
             file_paths = list(filter(lambda fp: not fp.endswith('-sources.jar') and not fp.endswith('-dev.jar') and not fp.endswith('-shadow.jar'),file_paths))
             if len(file_paths) == 0:
                 file_name = '*not found*'
